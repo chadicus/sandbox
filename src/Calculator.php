ea401b6bbd33fb1710be5a52efcb6a77e72bb809
chadicus <chadwickgray@gmail.com>
2015-08-28 13:34:15 -0400

    Add Calculator class

---
+++ src/Calculator.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Chadicus;
+
+final class Calculator
+{
+    /**
+     * Returns the sum of $x and $y
+     *
+     * @param integer $x The first integer.
+     * @param integer $y The second integer.
+     *
+     * @return integer The sum of $x and $y
+     */
+    public function add($x, $y)
+    {
+        return $x + $y;
+    }
+}
