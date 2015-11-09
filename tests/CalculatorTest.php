f98d0763c6ecd0abf02ec66398aa212e37b4268d
chadicus <chadwickgray@gmail.com>
2015-11-09 10:04:26 -0500

    Add multiply method

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -23,4 +23,18 @@ final class CalculatorTest extends \PHPUnit_Framework_TestCase
         $calculator = new Calculator();
         $this->assertSame(4, $calculator->add(3, 1));
     }
+
+    /**
+     * Verify basic functionality of multiply().
+     *
+     * @test
+     * @covers ::multiply
+     *
+     * @return void
+     */
+    public function multiply()
+    {
+        $calculator = new Calculator();
+        $this->assertSame(3, $calculator->multiply(3, 1));
+    }
 }
